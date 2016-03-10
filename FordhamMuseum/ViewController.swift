5e7f6a03a24ba442db070700d1c68170c4aab7fd
Michael Ceraso <mceraso@fordham.edu>
2016-03-10 13:23:53 -0500

    Test Fork Merge

--- FordhamMuseum/ViewController.swift
+++ FordhamMuseum/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("We merged the fork!")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
